9fada3b3a1978886d56e42c8fe5ae6bf7282b76d
Andres Dejesus <andresdejesus123@gmail.com>
2024-09-09 20:12:05 -0500

    login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,14 @@
-import { useState } from 'react'
+import {
+  useState,
+  type ChangeEvent,
+  type Dispatch,
+  type SetStateAction,
+} from 'react'
 import { usePostLogin, usePostLogout } from './api'
 import { Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+
+const MAX_USERNAME_LEN = 20
+const MAX_PASSWORD_LEN = 20
 
 export default function App() {
   return (
@@ -39,16 +47,40 @@ function Login() {
   const { mutate: postLogin } = usePostLogin()
   const { state } = useLocation()
   const navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onFieldChange = (
+    setField: Dispatch<SetStateAction<string>>,
+    maxLen: number,
+    regex: RegExp
+  ) => {
+    return (event: ChangeEvent<HTMLInputElement>) => {
+      const field = event.target.value
+      if (field.length <= maxLen && regex.test(field)) {
+        setField(field)
+      }
+    }
+  }
+
+  const onUsernameChange = onFieldChange(
+    setUsername,
+    MAX_USERNAME_LEN,
+    /^[a-zA-Z0-9_]*$/
+  )
+  const onPasswordChange = onFieldChange(
+    setPassword,
+    MAX_PASSWORD_LEN,
+    /^[^ ]*$/
+  )
 
   return (
     <div>
-      <button
-        onClick={() =>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
           postLogin(
-            {
-              username: 'foo',
-              password: 'bar',
-            },
+            { username, password },
             {
               onSuccess: ({ token }) => {
                 localStorage.setItem('token', token)
@@ -56,10 +88,20 @@ function Login() {
               },
             }
           )
-        }
+        }}
       >
-        Login
-      </button>
+        <label>
+          Username:
+          <input type='text' name='username' onChange={onUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type='password' name='password' onChange={onPasswordChange} />
+        </label>
+        <br />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }
