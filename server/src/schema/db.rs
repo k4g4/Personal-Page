a09bdc1c5a35ec486c69e6f9dd730e2141091fa8
Andres Dejesus <andresdejesus123@gmail.com>
2024-09-25 22:17:31 -0500

    remove indexjs

--- server/src/schema/db.rs
+++ server/src/schema/db.rs
@@ -1,8 +1,8 @@
+#![allow(unused)]
 use super::ids::{CardId, UserId};
 
 pub struct User {
     pub id: UserId,
-    #[allow(unused)]
     pub username: String,
     pub password_hash: String,
     pub password_salt_b64: String,
